3d54611c8d3e6396ea13d84062cea1e437ea4ecb
NigelB <nigel.blair@gmail.com>
2015-04-21 11:18:44 +1000

    Add ab08xx-spi header

---
+++ ab08xx-spi.h
@@ -0,0 +1,42 @@
+
+#ifndef __AB18XX_RTC_H__
+#define __AB18XX_RTC_H__
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define TIME_DATE 0x00
+#define ALARM_DATE 0x08
+#define ID0 0x28
+
+#define SPI_READ(offset) (0x7F & offset)
+#define SPI_WRITE(offset) (0x80 | offset)
+
+struct ab08xx_t {
+        struct spi_device       *spi;
+        struct rtc_device       *rtc;
+	u8			id_msb;
+	u8			id_lsb;
+	void (*original_pm_power_off)(void);
+	const struct attribute_group **groups;
+};
+
+
+static int ab08xx_read_time(struct device *dev, struct rtc_time *dt);
+
+static int ab08xx_set_time(struct device *dev, struct rtc_time *dt);
+
+static int ab08xx_get_alarm(struct device *dev, struct rtc_wkalrm *alm);
+
+static int ab08xx_set_alarm(struct device *dev, struct rtc_wkalrm *alm);
+
+static int ab08xx_proc(struct device *dev, struct seq_file *seq);
+
+static void ab08xx_shutdown(void);
+
+static struct ab08xx_t power_gizmo;
+
+#endif
